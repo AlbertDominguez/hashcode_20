4ebb1062e6abbbc3b9c6d28cf744b9b6d8218777
AlbertDominguez <albertadm99@gmail.com>
2020-02-21 11:06:33 +0100

    Updated shell script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 rm submission_*
 g++ -Wall -O2 -std=c++11 main.cc -o p.x
-./p.x < a_example.txt > submission_a.txt
-./p.x < b_read_on.txt > submission_b.txt
-./p.x < c_incunabula.txt > submission_c.txt
-./p.x < d_tough_choices.txt > submission_d.txt
-./p.x < e_so_many_books.txt > submission_e.txt
-./p.x < f_libraries_of_the_world.txt > submission_f.txt
+./p.x < input/a_example.txt > submission_a.txt
+./p.x < input/b_read_on.txt > submission_b.txt
+./p.x < input/c_incunabula.txt > submission_c.txt
+./p.x < input/d_tough_choices.txt > submission_d.txt
+./p.x < input/e_so_many_books.txt > submission_e.txt
+./p.x < input/f_libraries_of_the_world.txt > submission_f.txt
